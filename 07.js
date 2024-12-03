2e3c7afcf92dfdad9601e528d42d7f091bb47139
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-12-03 13:48:16 +0530

    Check Wth Arr&obj

---
+++ 07.js
@@ -0,0 +1,15 @@
+// let str = ' harash ';
+
+// let ans =str.split('').reverse().join('');
+// console.log(ans);
+
+
+function checkArray(elem){
+    return Array.isArray(elem);
+}
+
+
+console.log(checkArray([]));
+console.log(checkArray({}));
+
+
