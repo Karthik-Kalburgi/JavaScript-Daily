d10e8163291f7623b6823f0ee585bb24e1fdcfb4
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-12-11 18:31:10 +0530

    sumof100

---
+++ 13 SumandNumber.js
@@ -0,0 +1,35 @@
+// function twonm(a,b){
+//     while(true){
+//         if(a === 100 || b === 100){
+//             return true;
+//         }else if(a+b === 100){
+//             return true;
+//         }else{
+//             return false
+//         }
+//     }
+// }
+
+// console.log(twonm(100,0));
+// console.log(twonm(100,123));
+// console.log(twonm(10,100));
+// console.log(twonm(1,3));
+// console.log(twonm(50,50));
+// console.log(twonm(75,25));
+// console.log(twonm(45,90));
+// console.log(twonm(-30,-90));
+
+const isEuqalto100 = (a,b)=>(
+    a === 100 || b==100 || (a+b) === 100)
+   
+
+
+console.log(isEuqalto100(100,0));
+console.log(isEuqalto100(100,123));
+console.log(isEuqalto100(10,100));
+console.log(isEuqalto100(1,3));
+console.log(isEuqalto100(50,50));
+console.log(isEuqalto100(75,25));
+console.log(isEuqalto100(45,90));
+console.log(isEuqalto100(-30,-90));
+
